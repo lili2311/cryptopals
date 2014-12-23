6f7cc9f8ebedf2e95b8d4e8905800970fce02ad2
Ryan Janzen <rpwjanzen@gmail.com>
2014-12-23 15:46:36 -0700

    S1C1

---
+++ matasano/Script.fsx
@@ -0,0 +1,72 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Library1.fs"
+//#r @"C:\Users\Crystal\Documents\GitHub\cryptopals\matasano\packages\FSharpx.Collections.1.9.4\lib\net35\FSharpx.Collections.dll"
+
+open matasano
+//open FSharpx.Collections
+
+// Define your library scripting code here
+
+let hexToByte (h:char) : (byte) = 
+    match h with
+    | '0' -> 0uy
+    | '1' -> 1uy
+    | '2' -> 2uy
+    | '3' -> 3uy
+    | '4' -> 4uy
+    | '5' -> 5uy
+    | '6' -> 6uy
+    | '7' -> 7uy
+    | '8' -> 8uy
+    | '9' -> 9uy
+    | 'A' -> 10uy
+    | 'a' -> 10uy
+    | 'B' -> 11uy
+    | 'b' -> 11uy
+    | 'C' -> 12uy
+    | 'c' -> 12uy
+    | 'D' -> 13uy
+    | 'd' -> 13uy
+    | 'E' -> 14uy
+    | 'e' -> 14uy
+    | 'F' -> 15uy
+    | 'f' -> 15uy
+    | _ -> invalidArg "h" "Value must be [0-9,A-F]"
+
+let rec hexToBytes (cs:char list) : (byte list) =
+    match cs with
+    | [] -> []
+    | x::y::ys -> [(((hexToByte x) <<< 4) ||| hexToByte y)] @ (hexToBytes ys)
+
+let byteToBase64 (b:byte) : (char) =
+    match b with
+    // [0-25] -> [A-Z]
+    | l when l < 26uy ->  (char)(l + (byte)'A')
+    // [26-51] -> [a-z]
+    | l when l < 52uy ->  (char)(l - 26uy + (byte)'a')
+    // [52-61] -> [0-9]
+    | l when l < 62uy ->  (char)(l - 26uy  - 26uy + (byte)'0')
+    // 62 -> '+'
+    | 62uy ->  '+'
+    // 63 -> '/'
+    | 63uy ->  '+'
+    | _ -> invalidArg "b" (b.ToString()) // "Value must be [0-63] "
+
+let bytesToBase64Bytes (a:byte) (b:byte) (c:byte) : (byte list) =
+    let b0 = (a &&& 0b11111100uy) >>> 2
+    let b1 = ((a &&& 0b00000011uy) <<< 4) ||| ((b &&& 0b11110000uy) >>> 4)
+    let b2 = ((b &&& 0b00001111uy) <<< 2) ||| ((c &&& 0b11000000uy) >>> 6)
+    let b3 = c &&& 0b00111111uy
+    [b0;b1;b2;b3]
+
+let rec byteSeqToBase64 (bs:byte list) : (char list) =
+    match bs with
+    | []-> []
+    | x::y::z::zs-> ((bytesToBase64Bytes x y z) |> List.map byteToBase64) @ (byteSeqToBase64 zs)
+    //| x::y::ys -> ((bytesToBase64Bytes x y 0uy) |> List.map byteToBase64 |> Seq.take 3) @ ['=']
+    //| x::xs -> ((bytesToBase64Bytes x 0uy 0uy) |> List.map byteToBase64 |> Seq.take 2) @ ['=';'=']
+
+let hexToBase64 (hex:string) : (string) =
+    hex.ToCharArray() |> Array.toList |> hexToBytes |> Seq.toList |> byteSeqToBase64 |> System.String.Concat
