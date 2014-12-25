a5f2f799e9264a5fd42a898e6069a60d16a480f1
Ryan Janzen <rpwjanzen@gmail.com>
2014-12-24 23:16:17 -0700

    S1C3

--- matasano/Script.fsx
+++ matasano/Script.fsx
@@ -82,6 +82,74 @@ let hexToBase64 (hex:string) : (string) =
 
 // S1C2
 let fixedXor (s0:string) (s1:string) =
-    let bs0 = s0.ToCharArray() |> Array.toList |> hexToBytes 
-    let bs1 = s1.ToCharArray() |> Array.toList |> hexToBytes
+    let bs0 = s0 |> hexDecode
+    let bs1 = s1 |> hexDecode
     List.zip bs0 bs1 |> (List.map (fun (a,b) -> a ^^^ b)) |> hexEncode
+
+// S1C3
+let fixedXor2 (bs0:byte list) (bs1:byte list) : (byte list) =
+    List.zip bs0 bs1 |> (List.map (fun (a,b) -> a ^^^ b))
+
+let decodeHexEncodedSingleByteXorCipher (c:char) (s:string) =
+    let bytes = hexDecode s
+    let b = (byte)c
+    let source = List.init (bytes.Length) (fun _ -> b)
+    let xored = fixedXor2 source bytes
+    let chars = xored |> List.map (fun x -> (char)x)
+    chars |> System.String.Concat
+    
+let getDist (s:string) =
+    let total = s.Length
+    let letters = ['a' .. 'z']
+    let frequency = letters |> List.map (fun x -> (x,  s.ToLower().ToCharArray() |> Array.toList |> List.filter (fun z -> z.Equals(x)) |> List.length))
+    let dist = frequency |> List.map (fun (c,f) -> (c, (float)f / (float)total)) |> Map.ofList
+    dist
+
+let score (s:string) =
+    let normals = [
+        ('a',0.08167)
+        ('b',0.01492)
+        ('c',0.02782)
+        ('d',0.04253)
+        ('e',0.12702)
+        ('f',0.02228)
+        ('g',0.02015)
+        ('h',0.06094)
+        ('i',0.06966)
+        ('j',0.00153)
+        ('k',0.00772)
+        ('l',0.04025)
+        ('m',0.02406)
+        ('n',0.06749)
+        ('o',0.07507)
+        ('p',0.01929)
+        ('q',0.00095)
+        ('r',0.05987)
+        ('s',0.06327)
+        ('t',0.09056)
+        ('u',0.02360)
+        ('v',0.00978)
+        ('w',0.02360)
+        ('x',0.00150)
+        ('y',0.01974)
+        ('z',0.00074)] |> Map.ofList
+    let dist = getDist s
+    let theScore =
+        dist |>
+        Map.toList |>
+        List.map (fun (k,v) ->
+            normals.TryFind k |>
+            (fun o -> if Option.isSome o then o else Some 0.0) |>
+            Option.get |>
+            (-) v |>
+            (fun x -> x * x)) |>
+        List.sum
+    theScore
+
+let getScores (text:string) =
+    ['a' .. 'z'] @ ['A' .. 'Z'] |>
+    List.map (fun c -> decodeHexEncodedSingleByteXorCipher c text) |>
+    List.map (fun s -> (score s, s)) |>
+    List.sortBy (fun (s,r) -> s);;
+
+let theText = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
