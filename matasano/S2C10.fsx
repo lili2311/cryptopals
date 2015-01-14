d950ecb14e89600bed304fdb5e7fb09f8754ec45
Ryan Janzen <rpwjanzen@gmail.com>
2015-01-13 22:57:27 -0700

    Finish S2C10

--- matasano/S2C10.fsx
+++ matasano/S2C10.fsx
@@ -2,6 +2,44 @@
 open System.IO
 open System.Security.Cryptography
 
+let getByte (c:char) : (byte) =
+    match c with
+    | _ when c >= 'A' && c <= 'Z' -> byte(c) - byte('A')
+    | _ when c >= 'a' && c <= 'z' -> byte(c) - (byte('a') - 26uy)
+    | _ when c >= '0' && c <= '9' -> byte(c) - (byte('0') - 52uy)
+    | '+' -> 62uy
+    | '/' -> 63uy
+    | _ -> invalidArg "c" "c must be [0-63]"
+
+let getBytes (c0:char) (c1:char) (c2:char) (c3:char) : (byte seq) =
+    match (c0, c1, c2, c3) with
+    | (a,b,c,d) when c = '=' && d = '=' ->
+        let b0 = (getByte a <<< 2) ||| (getByte b >>> 4)
+        seq [ b0 ]
+
+    | (a,b,c,d) when d = '=' ->
+        let b0 = (getByte a <<< 2) ||| (getByte b >>> 4)
+        let b1 = (getByte b <<< 4) |||  (getByte c >>> 2)
+        seq [ b0; b1 ]
+
+    | (a,b,c,d) -> 
+        let b0 = (getByte a <<< 2) ||| (getByte b >>> 4)
+        let b1 = (getByte b <<< 4) ||| (getByte c >>> 2)
+        let b2 = (getByte c <<< 6) ||| (getByte d)
+        seq [ b0; b1; b2 ]
+
+let base64Decode (text:string) : (byte list) =
+    let rec decodeBytes chars =
+        match chars with
+        | [] -> []
+        | a::b::c::d::xs ->
+            let bytes = (getBytes a b c d) |> Seq.toList
+            bytes @ (decodeBytes xs)
+        | _ -> invalidArg "chars" "Bytes must be multiple of 4 in length"
+    let chars = text.ToCharArray() |> Array.toList
+    let bytes = decodeBytes chars
+    bytes
+
 let toChar (byte:byte) : (char) =
     char(byte)
 let toChars (bytes:byte seq) : (char seq) =
@@ -84,146 +122,42 @@ let encryptAesEcb (keyBytes:byte []) (iv:byte []) (bytes:byte []) =
     cryptoStream.FlushFinalBlock()
     data
 
-let plainText = "The quick brown fox jumped over the lazy dogs"
-let key = "YELLOW SUBMARINE" |> toBytes |> Seq.toArray
-let iv = "0000000000000000" |> toBytes |> Seq.toArray
-let cipherText = encryptAesEcb key iv (plainText |> toBytes |> Seq.toArray)
-let decrypted = decryptAesEcb key iv cipherText
-let decryptedText = decrypted |> toText
-
-//let decryptAesCbc (key:byte []) (iv:byte []) (bytes:byte seq) : (byte [] seq) =
-//    let blockSize = iv.Length
-//    let blocks = Seq.append [iv] (chunk blockSize bytes)
-//    let data =
-//        blocks
-//        |> Seq.map (fun block ->
-//            let iv = Array.zeroCreate<byte> 16
-//            (decryptAesEcb block iv key, block))
-//        |> Seq.windowed 2
-//        |> Seq.map (fun xs ->
-//            let (_, prevCipherText) = xs.[0]
-//            let (currentDecryptedText, _) = xs.[1]
-//            let plainText = xorBytes prevCipherText currentDecryptedText
-//            plainText)
-//    data
-
-
-//let cbcEncrypt (key:byte []) (iv:byte []) (bytes:byte seq) : (byte [] seq) =
-//    let blockSize = iv.Length
-//    let blocks = chunk blockSize bytes
-//    let encryptBlock (cipherText:byte []) (plainText:byte []) : (byte []) =
-//        let toEncrypt = xorBytes cipherText plainText
-//        encryptAesEcb key toEncrypt
-//    let data =
-//        blocks |> Seq.scan (encryptBlock) iv
-//        // 1st block result is the IV; ignore it
-//        |> Seq.skip 1
-//    data
-//
-//let encryptAesCbc (keyBytes:byte []) (iv:byte []) (bytes:byte []) =
-//    let blockSize = 16
-//    let blocks = chunk blockSize bytes |> Seq.toArray
-//    let encryptBlock (cipherText:byte []) (plainText:byte []) : (byte []) =
-//        let toEncrypt = xorBytes cipherText plainText
-//        let iv = Array.zeroCreate blockSize
-//        encryptAesEcb keyBytes iv toEncrypt
-//    let data =
-//        blocks |> Seq.scan (encryptBlock) iv
-//        // 1st block result is the IV; ignore it
-//        |> Seq.skip 1
-//    data
-//
-//
-//let cbcDecrypt (key:byte []) (iv:byte []) (bytes:byte seq) : (byte [] seq) =
-//    let blockSize = iv.Length
-//    let blocks = Seq.append [iv] (chunk blockSize bytes)
-//    let data =
-//        blocks
-//        |> Seq.map (fun c -> (xorDecrypt key c, c))
-//        |> Seq.windowed 2
-//        |> Seq.map (fun xs ->
-//            let (_, prevCipherText) = xs.[0]
-//            let (currentDecryptedText, _) = xs.[1]
-//            let plainText = xorBytes prevCipherText currentDecryptedText
-//            plainText)
-//    data
-//
-//
-//let key = "GAhjlIv53" |> toBytes |> Seq.toArray
-//let iv = "gdyrt[qv#" |> toBytes |> Seq.toArray
 //let plainText = "The quick brown fox jumped over the lazy dogs"
-//let plainTextBytes =
-//    plainText.ToCharArray()
-//    |> Array.map (fun c -> byte (c))
-//
-//let encryptedBytes =
-//    cbcEncrypt key iv plainTextBytes
-//    |> Seq.toArray
-//    |> Array.collect (fun x -> x)
-//let encryptedText = encryptedBytes |> toText |> escapeText
-//
-//let decryptedBytes =
-//    cbcDecrypt key iv encryptedBytes
-//    |> Seq.toArray
-//    |> Array.collect (fun x -> x)
-//
-//let decryptedText =
-//    decryptedBytes
-//    |> Array.map (fun b -> char(b))
-//    |> String.Concat
-//    |> escapeText
-//
-//let getByte (c:char) : (byte) =
-//    match c with
-//    | _ when c >= 'A' && c <= 'Z' -> byte(c) - byte('A')
-//    | _ when c >= 'a' && c <= 'z' -> byte(c) - (byte('a') - 26uy)
-//    | _ when c >= '0' && c <= '9' -> byte(c) - (byte('0') - 52uy)
-//    | '+' -> 62uy
-//    | '/' -> 63uy
-//    | _ -> invalidArg "c" "c must be [0-63]"
-//
-//let getBytes (c0:char) (c1:char) (c2:char) (c3:char) : (byte seq) =
-//    match (c0, c1, c2, c3) with
-//    | (a,b,c,d) when c = '=' && d = '=' ->
-//        let b0 = (getByte a <<< 2) ||| (getByte b >>> 4)
-//        seq [ b0 ]
-//
-//    | (a,b,c,d) when d = '=' ->
-//        let b0 = (getByte a <<< 2) ||| (getByte b >>> 4)
-//        let b1 = (getByte b <<< 4) |||  (getByte c >>> 2)
-//        seq [ b0; b1 ]
-//
-//    | (a,b,c,d) -> 
-//        let b0 = (getByte a <<< 2) ||| (getByte b >>> 4)
-//        let b1 = (getByte b <<< 4) ||| (getByte c >>> 2)
-//        let b2 = (getByte c <<< 6) ||| (getByte d)
-//        seq [ b0; b1; b2 ]
-//
-//let base64Decode (text:string) : (byte list) =
-//    let rec decodeBytes chars =
-//        match chars with
-//        | [] -> []
-//        | a::b::c::d::xs ->
-//            let bytes = (getBytes a b c d) |> Seq.toList
-//            bytes @ (decodeBytes xs)
-//        | _ -> invalidArg "chars" "Bytes must be multiple of 4 in length"
-//    let chars = text.ToCharArray() |> Array.toList
-//    let bytes = decodeBytes chars
-//    bytes
-//
-//let path = @"C:\Users\ryanj\Documents\GitHub\cryptopals\matasano\10.txt"
-//let doit path =
-//    let lines = System.IO.File.ReadAllLines path
-//    let bytes =
-//        lines
-//        |> Array.map (fun s -> base64Decode s |> List.toArray)
-//        |> Array.collect (fun x -> x)
-//    let key = "YELLOW SUBMARINE" |> toBytes |> Seq.toArray
-//    let iv = Array.create key.Length 0uy
-//    let decryptedBytes = cbcDecrypt key iv bytes
-//    let decryptedText =
-//        decryptedBytes
-//        |> Seq.collect (fun x -> x)
-//        |> toText
-//        |> escapeText
-//    decryptedText
+//let key = "YELLOW SUBMARINE" |> toBytes |> Seq.toArray
+//let iv = "0000000000000000" |> toBytes |> Seq.toArray
+//let cipherText = encryptAesEcb key iv (plainText |> toBytes |> Seq.toArray)
+//let decrypted = decryptAesEcb key iv cipherText
+//let decryptedText = decrypted |> toText
+
+let decryptAesCbc (key:byte []) (iv:byte []) (bytes:byte seq) : (byte [] seq) =
+    let blockSize = iv.Length
+    let blocks = Seq.append [iv] (chunk blockSize bytes)
+    let data =
+        blocks
+        |> Seq.map (fun block ->
+            let iv = Array.zeroCreate<byte> key.Length
+            (decryptAesEcb key iv block, block))
+        |> Seq.windowed 2
+        |> Seq.map (fun xs ->
+            let (_, prevCipherText) = xs.[0]
+            let (currentDecryptedText, _) = xs.[1]
+            let plainText = xorBytes prevCipherText currentDecryptedText
+            plainText)
+    data
+
+let path = @"C:\Users\ryanj\Documents\GitHub\cryptopals\matasano\10.txt"
+let result =
+    let lines = System.IO.File.ReadAllLines path
+    let bytes =
+        lines
+        |> Array.map (fun s -> base64Decode s |> List.toArray)
+        |> Array.collect (fun x -> x)
+    let key = "YELLOW SUBMARINE" |> toBytes |> Seq.toArray
+    let iv = Array.create key.Length 0uy
+    let decryptedBytes = decryptAesCbc key iv bytes
+    let decryptedText =
+        decryptedBytes
+        |> Seq.collect (fun x -> x)
+        |> toText
+        |> escapeText
+    decryptedText
